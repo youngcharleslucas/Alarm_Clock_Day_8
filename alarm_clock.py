01e836c66132a728d663d912d8f3866fbf9d1075
Your Name <charles.luas.young@gmail.com>
2021-10-12 10:44:02 -0600

    Working on time

---
+++ alarm_clock.py
@@ -0,0 +1,20 @@
+class AlarmClock:
+    def __init__(self):
+        self.set_alarm = ""
+        self.toggle = ""
+        self.print_time = ""
+        self.print_settime = ""
+
+    def clock (self):
+        import datetime
+        time = datetime.datetime()
+        current_time = time.strftime("%X")
+        return current_time
+
+        
+
+    def set_alarm (self):
+
+
+
+
