ddd75197dd35006d7c86dfe92b4cdf4469cbc649
Your Name <charles.luas.young@gmail.com>
2021-10-12 11:32:22 -0600

    Testing print of alarm set

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,6 +1,6 @@
 class AlarmClock:
     def __init__(self):
-        self.set_alarm = ""
+        self.set_alarm = []
         self.toggle = ""
         self.print_time = ""
         self.print_settime = ""
@@ -10,10 +10,21 @@ class AlarmClock:
         time = datetime.datetime.now()
         current_time = time.strftime("%X")
         return current_time
+      
+    def set_alarm (self):
+        self.set_alarm = []
+        proper_time = True
+        while True:
+            hour_alarm = int(input("What hour would you like to set the alarm? Use 2 digits (00), and 24hr time."))
+            min_alarm = int(input("What minute would you like to set the alarm? Use 2 digits (00)."))
+            if hour_alarm <= 24 and hour_alarm >= 00 and min_alarm <= 60:
+                self.set_alarm.append(hour_alarm)
+                self.set_alarm.append(min_alarm)
+                proper_time = False
+            return self.set_alarm 
 
-        
+            
 
-    # def set_alarm (self):
 
 
 
